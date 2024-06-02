d570c02ab291468a48a8d7732ad23cfc149f0f06
Lesedi <lesedimautsoa2@gmail.com>
2024-06-02 18:20:35 +0200

    update

--- api/app.js
+++ api/app.js
@@ -1,13 +1,16 @@
 import express from "express"
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
 import dotenv from "dotenv";
 
 const app = express();
+
+
 dotenv.config();
 
-
+app.use(cors({origin: process.env.CLIENT_URL, credentials: true}))
 app.use(express.json())
 app.use(cookieParser())
 
